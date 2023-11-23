928369c9114a55545d7d546a45fce91b9a1aeb02
bulacoola <brandenb689@gmail.com>
2023-11-22 23:02:38 -0500

    app.js, some routes, mongo connections

--- config/settings.js
+++ config/settings.js
@@ -1,5 +1,5 @@
 // YOU WILL NEED TO CHANGE THE DB NAME TO MATCH THE REQUIRED DB NAME IN THE ASSIGNMENT SPECS!!!
 export const mongoConfig = {
     serverUrl: 'mongodb://127.0.0.1:27017/',
-    // database: 'Branden_Bulatao_lab4'
+    database: 'Fast_Family_Finder'
   };
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,13 @@
 // 
+import lineRoutes from "lines.js";
+
+const constructorMethod = (app) => {
+    // app.use('/events', eventsRoutes);
+    // app.use('/attendees', attendeesRoutes);
+    app.use('/', lineRoutes);
+  
+    app.use('*', (req, res) => {
+      res.status(404).json({error: 'Not found'});
+    });
+  };
+export default constructorMethod;// 
