ccb16510b4dbda9b9922ee3c2c03b25d528d8f0f
JeddAlcalde <jedd.alcalde@gmail.com>
2023-12-07 13:35:20 -0500

    createLines() and update to users.js Minor validation fixes to data/lines.js. Addition of createLine() method, note lineHead must be a registered user BEFORE becoming the head of a line. users.js added methods. Awaiting updates to validators.js, sent required methods to Isabel.

--- data/users.js
+++ data/users.js
@@ -1,1 +1,32 @@
 // I don't know if we need this. Not hundred percent. - Branden Bulatao
+import {users} from '../config/mongoCollections.js';
+import {ObjectId} from 'mongodb';
+import user from './users.js'
+import validation from '../validators.js';
+import bcrypt from 'bcrypt';
+
+const exportedMethods = {
+    async getAllUsers(){
+        const userCollection = await users();
+        return await userCollection.find({}).toArray();
+    },
+    async getUserById(id) {
+        id = validation.validId(id);            //subject to change
+        const userCollection = await lines();
+        const user = await userCollection.findOne({_id: new ObjectId(id)});
+        if (!user) throw 'Error: User not found';
+        return user;
+    },
+    async getUserByEmail(email){
+        email = validation.validEmail(email);        //subject to change
+        const userCollection = await users();
+        const user = await userCollection.findOne({_email: email});
+        if (!user) throw 'Error: Line not found';
+        return user;
+    },
+    async createUser(userName, userBio, firstName, lastName, email, password, major, gradYear, big, littles, links){
+        //subject to change
+    }
+}
+
+export default exportedMethods;
