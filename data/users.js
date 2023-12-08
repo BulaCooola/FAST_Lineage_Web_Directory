542dc9539c23518ef0b12e0f002b07d1a8b9d5e1
spayba <seanpayba@gmail.com>
2023-12-07 23:22:32 -0500

    create function registerUser

--- data/users.js
+++ data/users.js
@@ -24,9 +24,39 @@ const exportedMethods = {
         if (!user) throw 'Error: Line not found';
         return user;
     },
-    async createUser(userName, userBio, firstName, lastName, email, password, major, gradYear, big, littles, links){
+    async registerUser(userName, userBio, firstName, lastName, email, password, confirmPassword, major, gradYear, big, littles, links){
         //subject to change
-    }
+        const userCollection = await users();
+        const findUserName = await userCollection.findOne({userName: userName});
+        if (findUserName) {
+            throw `username already exists, pick another`
+        }
+
+        if(password !== confirmPassword){
+            throw 'passwords must be the same';
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 16);
+        let newUser = { 
+            userName:userName,
+            userBio: userBio,
+            firstName: firstName,
+            lastName: lastName,
+            email:email,
+            password: hashedPassword,
+            major: major,
+            gradYear: gradYear,
+            big:big,
+            little:littles,
+            links:links
+        }
+
+        let insertInfo = await userCollection.insertOne(newUser);
+        if (!insertInfo.acknowledged || !insertInfo.insertedId) {
+            throw 'Could not add user';
+        }
+        return {insertedUser:true};
+        }
 }
 
 export default exportedMethods;
