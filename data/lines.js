04f2f4ffc91450138e45799020de10087d1dd99f
BulaCooola <100243206+BulaCooola@users.noreply.github.com>
2023-12-17 22:30:13 -0500

    cleaning console.logs
    cleaned unecessary console.logs

--- data/lines.js
+++ data/lines.js
@@ -146,8 +146,6 @@ const exportedMethods = {
     async createMessage(userName, msg, line) {
         const linesCollection = await lines();
 
-        console.log('data stage 1');
-
         try {
             userName = validation.validString(userName, 'UserName');
             msg = validation.validString(msg, 'Message');
@@ -159,7 +157,6 @@ const exportedMethods = {
             throw `${e}`
         }
 
-        console.log('data stage 2');
         try {
             // get the line4
             const getLine = await linesCollection.findOne({ lineName: line })
@@ -167,9 +164,6 @@ const exportedMethods = {
                 // 404
                 throw `Error: Line not found`;
             }
-            console.log('data stage 2.1');
-            console.log(getLine.messages.length)
-
 
             const newMessage = {
                 _id: getLine.messages.length,
@@ -177,8 +171,6 @@ const exportedMethods = {
                 userName: userName,
                 text: msg
             }
-
-            console.log('data stage 2.2');
 
             const result = await linesCollection.updateOne(
                 { lineName: line },
@@ -188,8 +180,6 @@ const exportedMethods = {
             if (result.matchedCount !== 1) {
                 throw `Error: Unable to add message to database.`;
             }
-
-            console.log('data stage 2.3');
 
         } catch (e) {
             throw `${e}`;
